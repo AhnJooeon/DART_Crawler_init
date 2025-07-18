521c8c1f8f899da70e214f10f8ab7892acd72ebd
AhnJooeon <136285002+AhnJooeon@users.noreply.github.com>
2025-07-18 16:27:12 +0900

    Update DART_Crawler_02.py
    Add Read & Write

--- DART_Crawler_02.py
+++ DART_Crawler_02.py
@@ -1,5 +1,5 @@
 # from tkinter.tix import Select
-
+from requests.packages import target
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -22,10 +22,10 @@ def init_url():
 
 def input_num(num):
     # 사업자등록번호 num parsing
-    num_li = num.split('-')
-    num_1 = num_li[0]
-    num_2 = num_li[1]
-    num_3 = num_li[2]
+    # num_li = num.split('-')
+    num_1 = num[0:3]
+    num_2 = num[3:5]
+    num_3 = num[5:]
     data = []
     try:
         select_element = driver.find_element(By.CLASS_NAME, 'w10')
@@ -66,7 +66,9 @@ def input_num(num):
             cells = row.find_elements(By.TAG_NAME, 'td')
             cell_texts = [cell.text.strip() for cell in cells]
             if cell_texts:
-                data.append(cell_texts)
+                cell = cell_texts[0].replace("['", '')
+                cell = cell.replace("']", '')
+                data.append(cell)
         # print(data)
 
     except Exception as ex:
@@ -76,16 +78,41 @@ def input_num(num):
 
 # 전체 게시글을 담을 리스트
 all_rows = []
-tmp_com = ['120-81-08227', '124-81-00998']
+
+# 사업자번호 목록
+target_df = pd.read_csv('./Target_list.csv', encoding='euc-kr')
+# tmp_com = ['120-81-08227', '124-81-00998']
+tmp_com = target_df['사업자번호'].to_list()
+print(tmp_com)
+
+# result
+try:
+    result_df = pd.read_csv('./result.csv', encoding='utf-8-sig')
+except:
+    col = ['회사이름', '영문명', '공시회사명', '종목코드', '대표자명', '법인구분', '법인등록번호', '사업자등록번호', '주소',
+           '홈페이지', 'IR홈페이지', '전화번호', '팩스번호', '업종명', '설립일', '결산월']
+    result_df = pd.DataFrame(columns=col)
+
+# selenium init
 init_url()
 
 # Read list
 for i in range(len(tmp_com)):
-    tmp = input_num(tmp_com[i])
-    print(tmp)
-    all_rows.append(tmp)
+    # 크롤링 여부 확인
+    tmp_num = str(tmp_com[i])
 
-df = pd.DataFrame(all_rows)
-print(df)
+    num = f"{tmp_num[0:3]}-{tmp_num[3:5]}-{tmp_num[5:]}"
+    if num in result_df['사업자등록번호'].values:
+        print("Check")
+    else:
+        tmp = input_num(tmp_num)
+        print(f"len : {len(tmp)} / {tmp}")
+        if len(tmp) > 0:
+            result_df.loc[len(result_df)] = tmp
+            result_df.to_csv('./result.csv', index=False, encoding='utf-8-sig')
+        else:
+            result_df.loc[len(result_df), '사업자등록번호'] = num
 
-df.to_csv('./tmp.csv', index=False)
+
+
+print("finish")
